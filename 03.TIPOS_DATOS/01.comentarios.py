943cc50660e133836dd9783c3b2589c6504df317
carrasco03-noemy <carrascon444@gmail.com>
2026-05-21 09:03:25 -0500

    clases iniciales de python

---
+++ 03.TIPOS_DATOS/01.comentarios.py
@@ -0,0 +1,11 @@
+# comentarios de una sola linia
+"""
+comentarios multi linia
+tambien se les conose como docstring
+"""
+'''
+comentarios de multilinia con comillas simples
+'''
+"""los comentarios sirvrn para poder explicar
+ el funcionanmiento de siertas linias de codigo como funcion y
+   aplicacion o que dato recibe y que dato devuelve"""
